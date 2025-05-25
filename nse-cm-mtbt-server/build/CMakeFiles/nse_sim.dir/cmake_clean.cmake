30089c5778b0943b360623f484f86b34f28f8309
Thilak T P Linux <tpthilaktp@gmail.com>
2025-05-25 09:52:03 +0000

    Added a CMakefile along with the build.sh script to create the binary using CMake instead of the make file

---
+++ nse-cm-mtbt-server/build/CMakeFiles/nse_sim.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "/home/ovrteq/learning/dev/nse-cm-mtbt-sim/nse-tbt-feed-ai-trading/nse-cm-mtbt-server/bin/nse_sim"
+  "/home/ovrteq/learning/dev/nse-cm-mtbt-sim/nse-tbt-feed-ai-trading/nse-cm-mtbt-server/bin/nse_sim.pdb"
+  "CMakeFiles/nse_sim.dir/src/common.cpp.o"
+  "CMakeFiles/nse_sim.dir/src/common.cpp.o.d"
+  "CMakeFiles/nse_sim.dir/src/config_parser.cpp.o"
+  "CMakeFiles/nse_sim.dir/src/config_parser.cpp.o.d"
+  "CMakeFiles/nse_sim.dir/src/log.cpp.o"
+  "CMakeFiles/nse_sim.dir/src/log.cpp.o.d"
+  "CMakeFiles/nse_sim.dir/src/multicast.cpp.o"
+  "CMakeFiles/nse_sim.dir/src/multicast.cpp.o.d"
+  "CMakeFiles/nse_sim.dir/src/nse_feed_gen.cpp.o"
+  "CMakeFiles/nse_sim.dir/src/nse_feed_gen.cpp.o.d"
+  "CMakeFiles/nse_sim.dir/src/simulate_mtbt_values.cpp.o"
+  "CMakeFiles/nse_sim.dir/src/simulate_mtbt_values.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nse_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
